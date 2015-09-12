88ee93b08e79ef1742123cded61c39f300a3924c
azilnoor <azilnoor.salleh@vads.com>
2015-09-13 02:57:39 +0800

    R code for plot2
    Create plot2

---
+++ Plot2.R
@@ -0,0 +1,11 @@
+#read and tabulate data from local
+datafile <- "household_power_consumption.txt"
+data <- read.table(datafile, header=TRUE, sep=";", stringsAsFactors = FALSE,dec = ".")
+datasubset <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#Plot2Code
+datetime <- strptime(paste(datasubset$Date, datasubset$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(datasubset$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
